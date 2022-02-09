3c3318f8af7601752c7603a40a8b3fe6219ac1a0
Ilya Khazan <p9067700518@gmail.com>
2022-02-09 21:27:09 +0300

    Компонент Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,9 @@
-import { useCallback, useState, useEffect } from 'react'
+import * as React from "react";
+
+import {useCallback, useState, useEffect} from 'react'
 import cn from 'classnames'
 
-import { Values } from '../../types'
+import {Values} from '../../types'
 
 import styles from './styles.css'
 
@@ -9,25 +11,58 @@ const Variant = {
   TEXT: 'text',
   CONTAINED: 'contained',
   OUTLINED: 'outlined',
-} as const
-
-type Props = {
-  color?: 'primary' | 'accent' | 'secondary'
-  variant?: Values<typeof Variant>
-  text: string
-  width?: 'auto' | '35' | '70' | '120' | '150' | '180'
-  pressed?: boolean
 }
 
+const Color = {
+  PRIMARY: 'primary',
+  ACCENT: 'accent',
+  SECONDARY: 'secondary',
+  BLUE: 'blue',
+
+}
+
+const Size = {
+  XXS: "xxs",
+  XS: "xs",
+  S: "s",
+  M: "m",
+  L: "l",
+  XL: "xl",
+}
+
+type Props = {
+  color?: Values<typeof Color>
+  variant?: Values<typeof Variant>
+  text?: string
+  width?: string
+  size?: Values<typeof Size>
+  pressed?: boolean
+  disabled?: boolean
+  startIcon?: object
+  endIcon?: object
+  ariaLabel?: string
+  onClick?: any
+}
+
+
+
+
 export const Button = ({
-  color = 'primary',
-  variant = Variant.TEXT,
-  text,
-  width = '120',
-  pressed = false,
-}: Props) => {
+                         color = Color.PRIMARY,
+                         variant = Variant.TEXT,
+                         text,
+                         width = "auto",
+                         pressed = false,
+                         disabled = false,
+                         size = Size.M,
+                         startIcon,
+                         endIcon,
+                         ariaLabel,
+                         onClick,
+                       }: Props) => {
   const [isHovered, setIsHovered] = useState(false)
   const [isPressed, setIsPressed] = useState(pressed)
+  const [isDisabled, setDisabled] = useState(disabled)
 
   const handleMouseDown = useCallback(() => {
     setIsPressed(true)
@@ -56,15 +91,23 @@ export const Button = ({
         [styles['button--hover']]: isHovered,
         [styles[`button--color_${color}`]]: true,
         [styles[`button--variant_${variant}`]]: true,
-        [styles[`button--width_${width}`]]: true,
+        [styles[`button--size_${size}`]]: true,
       })}
+      style={{width: `${width}`}}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-    >
+      disabled={disabled}
+      aria-label={ariaLabel}
+      onClick={onClick}
+    >   <span className={cn(styles.icon, {
+      [styles[`icon--start`]]: true,
+    })}>{startIcon}</span>
       {text}
-      <span className={styles.icon}></span>
+      <span className={cn(styles.icon, {
+        [styles[`icon--end`]]: true,
+      })}>{endIcon}</span>
     </button>
   )
 }
